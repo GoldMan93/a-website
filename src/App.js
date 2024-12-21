19271ac91d30bf01da611ec97eafc0d96f98b555
Tomato Egg <jasonpsc@qq.com>
2024-12-21 18:49:12 +1000

    remove ncessearry

--- src/App.js
+++ src/App.js
@@ -45,15 +45,13 @@ function App() {
   return (
     <div className="d-flex" style={{ height: "100vh" }}>
       {/* Sidebar */}
-      <div className="bg-dark text-white sidebar">
-        <h4 className="p-3 text-center bg-warning text-dark">
-          STORM Industries
-        </h4>
+      <div className="sidebar">
+        <h4 className="p-3 text-center navbar">STORM Industries</h4>
         <ul className="list-group list-group-flush">
           {menuItems.map((item, index) => (
             <li
               key={index}
-              className="list-group-item list-group-item-action bg-dark text-white d-flex align-items-center"
+              className="list-group-item list-group-item-action d-flex align-items-center"
               onClick={() => setActivePage(item.name)}
               style={{ cursor: "pointer" }}>
               <span className="me-2">{item.icon}</span>
@@ -64,19 +62,30 @@ function App() {
       </div>
 
       {/* Main Content */}
-      <div className="flex-grow-1 p-3">
-        {/* Top Navbar */}
-        <div className="d-flex justify-content-between align-items-center bg-warning p-2">
-          <div>
-            <span className="me-3">🔔</span>
-            <span>🛠️</span>
+      <div className="flex-grow-1">
+        <div className="navbar p-2 d-flex justify-content-between align-items-center">
+          {/* Navbar Title */}
+          <h5 className="mb-0"></h5>
+
+          {/* Notification Icon and User Avatar */}
+          <div className="d-flex align-items-center">
+            <span
+              className="me-3"
+              style={{ fontSize: "1.5rem", cursor: "pointer" }}>
+              🔔
+            </span>{" "}
+            {/* Notification Icon */}
+            <img
+              src="https://via.placeholder.com/40" /* Replace with your avatar image URL */
+              alt="User Avatar"
+              className="rounded-circle"
+              style={{ width: "40px", height: "40px", cursor: "pointer" }}
+            />
           </div>
         </div>
 
         {/* Dynamic Content */}
-        <div className="content-area p-4 bg-dark text-white">
-          {renderPage()}
-        </div>
+        <div className="content-area">{renderPage()}</div>
       </div>
     </div>
   );
